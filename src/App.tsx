2e2ebc32da8a25ab5cf3df097ac7f233916aadd8
M.Babayi574@Gmail.Com <M.Babayi574@Gmail.Com>
2025-04-23 11:40:58 +0330

    Add Avatar component with image and name details

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import avatarImage from "./assets/Avatars/Avatar.jpeg";
 
 function App() {
   return (
@@ -6,8 +7,15 @@ function App() {
       <div className="MetroUi-Container">
         <div className="MetroUi-Header">
           <h1>بِسْمِ اللهِ الرَّحْمَنِ الرَّحِيمِ</h1>
-          <div className="Avatar"></div>
+          <div className="Avatar">
+            <img src={avatarImage} alt="Avatar" />
+            <div className="Avatar-Name">
+              <h2>محمدرضا</h2>
+              <h3>بابایی</h3>
+            </div>
+          </div>
         </div>
+        <div className="MetroUi-Body">{/* Metro UI Tiles */}</div>
       </div>
     </div>
   );
